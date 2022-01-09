1212fe46ef175dfb5989a240a0100d624c897983
CristianSalcedo <97403899+CristianSalcedo@users.noreply.github.com>
2022-01-09 12:58:53 -0500

    Reto Semana 1 solucion
    Codigo: 2201775
    Nombre : Cristian Rolando Salcedo Quintero
    Grupo : H2
    Profesor : David Cotes

---
+++ Semana1/Reto/Entregas/lunes_2201775/CALCULO DE NUMEROS.py
@@ -0,0 +1,5 @@
+numero = input("Ingrese el valor del numero que se va a ejecutar: ")
+
+calculo = int(numero) + int(str(numero+numero)) + int(str(numero+numero+numero))
+
+print("El valor calculado es " + str(calculo))
---
+++ Semana1/Reto/Entregas/lunes_2201775/PRIMER PROGRAMA.py
@@ -0,0 +1,24 @@
+# NECESITO LA EDAD DEL USUARIO PARA PRODUCIR LA EDAD QUE TENDRA EL SIGUIENTE AÑO
+# INPUT: Edad de la persona ? Restricciones: edad > 0
+
+# OUTPUT: Edad de la persona +1
+
+# 1. Pedir la edad al usuario
+while True:
+    try:
+        edad_del_usuario = int(input("Ingrese su edad: "))
+        if edad_del_usuario > 0:
+          break
+        else:
+            print("Ingrese una edad valida")
+    except:
+        print("Ingrese una edad valida")
+
+   
+    
+# 2. Calcular la edad del usuario en el siguiente año (edad + 1)
+edad_mas_uno = int(edad_del_usuario) + 1
+
+# 3. Mostar el resultado al usuario
+print("Su edad dentro de un año será: " + str(edad_mas_uno))
+#OUTPUT: Edad de la persona +1
